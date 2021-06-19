df4f1d1a8938c51746b820d7d40fab02765312b8
Kyler Rynear <kylerrynear@Kylers-MBP.socal.rr.com>
2021-06-19 16:25:59 -0700

    added shelf class file

--- candy.rb
+++ candy.rb
@@ -1,5 +1,29 @@
 class Candy
-   def initialize(name, shelved)
-      @name = name
+   attr_accessor :name, :shelved
+   def initialize(name)
+      @candy_name = name
       @shelved = 0
+   end
+   def print_name
+      puts @candy_name
+   end
+   def is_shelved
+      if @shelved == 0
+         puts "not shelved"
+      elsif @shelved == 1
+         puts "shelved"
+      end
+   end
+   def shelve
+      @shelved = 1
+   end
+   def unshelve
+      @shelved = 0
+   end
 end
+
+candy1 = Candy.new("twix")
+candy1.print_name
+candy1.is_shelved
+candy1.shelve
+candy1.is_shelved
